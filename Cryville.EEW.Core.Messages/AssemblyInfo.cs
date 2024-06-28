ba64a0e54781c9537cc658d7143755bb69dbc9c6
PopSlime <popslime@qq.com>
2024-06-28 21:04:11 +0800

    Add neutral language warning suppression

--- Cryville.EEW.Core.Messages/AssemblyInfo.cs
+++ Cryville.EEW.Core.Messages/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Cryville.EEW.Core")]
+[assembly: SuppressMessage("Performance", "CA1824", Justification = "Varied neutral cultures")]
