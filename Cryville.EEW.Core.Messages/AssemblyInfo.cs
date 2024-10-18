846889e5464f2b7c44e4bdae00e9d999ed4e3444
PopSlime <popslime@qq.com>
2024-10-18 20:51:21 +0800

    Refactor core messages into JSON

--- Cryville.EEW.Core.Messages/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Cryville.EEW.Core")]
-[assembly: SuppressMessage("Performance", "CA1824", Justification = "Varied neutral cultures")]
